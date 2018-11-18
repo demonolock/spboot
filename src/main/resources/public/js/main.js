6724591bfb91627c233825d0f7fd371d0f5cd2e0
victoria.shepard <victoria.shepard@team.wrike.com>
2018-11-18 22:21:23 +0300

    working version

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,9 +1,12 @@
 var app = angular.module("spboot", []);
 
-app.controller("AppCtrl", function($scope, $http) {
-    $scope.websites = [];
-
-    $http.get('http://localhost:8090/api/stackoverflow').success(function(data) {
-        $scope.websites = data;
-    })//Future, promise
+app.controller('AppCtrl', function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8090/api/stackoverflow'
+    }).then(function (response) {
+        $scope.websites = response.data;
+    }, function (error) {
+        console.log("error = " + error.status);
+    });
 });
